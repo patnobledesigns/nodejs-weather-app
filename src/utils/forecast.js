6d68ed75bcb19a0ac0dc8218ed55f3d7f0eeb53c
Kalu Patrick Okechukwu <kalupatrick94@gmail.com>
2021-06-14 19:53:58 +0100

    weather update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}, It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`);
+            callback(undefined, `${response.body.current.weather_descriptions[0]}, It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out. The humidity is ${response.body.current.humidity}.`);
         }
     });
 };
